3500267bb45c7632b992c1ee8c9af6a0757014ac
Rohan Patel <rohanp4636@gmail.com>
2017-06-04 20:23:21 -0400

    Added GET /todo/:id route

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -59,7 +59,7 @@ describe('POST /todos', () => {
   })
 });
 
-describe('POST /todos', () => {
+describe('GET /todos', () => {
   it('should get all todos', (done) => {
     request(app)
       .get('/todos')
